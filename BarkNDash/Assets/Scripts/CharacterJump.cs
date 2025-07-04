a5dfdf928b25b5905e26e78a2e5ab95a299d762d
Vilde Hagtvedt <54098897+vildeHagt@users.noreply.github.com>
2025-07-04 21:07:32 +0200

    jump

--- BarkNDash/Assets/Scripts/CharacterJump.cs
+++ BarkNDash/Assets/Scripts/CharacterJump.cs
@@ -6,14 +6,17 @@ public class CharacterJump : MonoBehaviour
     private PlayerInput playerInput;
     private InputAction touchPressAction;
     private Rigidbody2D rb;
+    private Animator animator;
 
     public float jump;
+    public bool isOnGround = true;
 
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
         touchPressAction = playerInput.actions["TouchPress"];
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnEnable()
@@ -28,9 +31,22 @@ public class CharacterJump : MonoBehaviour
 
     private void TouchPressed(InputAction.CallbackContext context)
     {
-        if (rb != null)
+        if (rb != null && isOnGround == true)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jump);
+            isOnGround = false;
+            animator.SetBool("isJumping", !isOnGround);
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOnGround = true;
+        animator.SetBool("isJumping", !isOnGround);
+    }
+
+    private void FixedUpdate()
+    {
+        animator.SetFloat("yVelocity", rb.linearVelocity.y);
     }
 }
